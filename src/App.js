6c1b0377132a7761cf64ba3fc0d523b08c3f05f6
camilocruzv <camilocruzv@users.noreply.github.com>
2019-07-24 23:47:23 -0500

    Login view

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <center>
-        <div class="card w-75">
-          <div class="card-body">
-            <h5 class="card-title">Tweet</h5>
-            <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-              <div class="input-group">
-                <input type="text" class="form-control bg-light border-0 small" placeholder="¿Qué está pasando?" aria-label="Search" aria-describedby="basic-addon2">
-                </input>
-              </div>
-            </form>
-            <a href="#" class="btn btn-primary">Tweetear</a>
+      <div class="login-form">
+        <form action="/examples/actions/confirmation.php" method="post">
+          <h2 class="text-center">Ingresar</h2>
+          <div class="text-center social-btn">
+            <a href="#" class="btn btn-primary btn-block"><i class="fa fa-facebook"></i> Ingresar con <b>Facebook</b></a>
+            <a href="#" class="btn btn-info btn-block"><i class="fa fa-twitter"></i> Ingresar con <b>Twitter</b></a>
+            <a href="#" class="btn btn-danger btn-block"><i class="fa fa-google"></i> Ingresar con <b>Google</b></a>
           </div>
-        </div>
-      </center>
+          <div class="or-seperator"><i>o</i></div>
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon"><i class="fa fa-user"></i></span>
+              <input type="text" class="form-control" name="username" placeholder="Usuario" required="required"></input>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+              <input type="password" class="form-control" name="password" placeholder="Contraseña" required="required"></input>
+            </div>
+          </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-success btn-block login-btn">Ingresar</button>
+          </div>
+
+        </form>
+
+      </div>
+
     </div>
   );
 }
